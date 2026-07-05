4f54bf6136aa5bf7206b45d3868d818e9b6180e7
Fivewayses <fivenumnoii@gmail.com>
2026-07-05 12:23:02 +0700

    Updated error system.

--- error/error.h
+++ error/error.h
@@ -31,19 +31,45 @@ std::string GetError(uint32_t index = 0)
 	return err_buffer;
 }
 
-void PushError(error_t err)
+void PushError(
+	const char *msg,
+	int64_t p1 = 0, int64_t p2 = 0,
+	int64_t p3 = 0, int64_t p4 = 0
+)
 {
-	err.valid = true;
+	error_t err = { msg, p1, p2, p3, p4, true };
 	
 	// Shift the error stack.
 	for (int i = ERROR_STACK_SIZE - 1; i > 0; --i) {
-		error_stack[i] = error_stack[i-1];
+		error_t prev = error_stack[i-1];
+		if (!prev.valid) return;
+		
+		error_stack[i] = prev;
 	}
 	
 	error_stack[0] = err;
 }
 
+void ClearError()
+{
+	error_t &err = error_stack[0];
+	int32_t i = 0;
+	
+	while (err.valid) {
+		err.valid = false;
+		err = error_stack[++i];
+	}
+}
+
+uint32_t GetNumErrors()
+{
+	int32_t i = 0;
+	while (error_stack[i].valid) ++i;
+	
+	return i;
+}
+
 inline void RedirectSDLError()
 {
-	PushError({ SDL_GetError() });
+	PushError(SDL_GetError());
 }
