c12e7780df6ee2d796d7bbf5ce7d2e1fe52fbc4d
Fivewayses <fivenumnoii@gmail.com>
2026-07-15 19:17:14 +0700

    Added copyright notices.

--- error/error.h
+++ error/error.h
@@ -1,3 +1,25 @@
+/**
+ * SynthCi_SDL2 - Fivewayses (TM)
+ *
+ * ===============================
+ *
+ * MIT License
+ * 
+ * Copyright (C) Fivewayses (2026).
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+ * (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ * 
+ *  [*] The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * 
+ *  [*] THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ *      FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *      CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 
 #include <cstdint>
@@ -15,7 +37,7 @@ struct error_info_t {
 
 static error_info_t error_stack[ERROR_STACK_SIZE];
 
-std::string GetError(uint32_t index = 0)
+static std::string GetError(uint32_t index = 0)
 {
 	if (index >= ERROR_STACK_SIZE) return "";
 	
@@ -31,7 +53,7 @@ std::string GetError(uint32_t index = 0)
 	return err_buffer;
 }
 
-void PushError(
+static void PushError(
 	const char *msg,
 	int64_t p1 = 0, int64_t p2 = 0,
 	int64_t p3 = 0, int64_t p4 = 0
@@ -50,7 +72,7 @@ void PushError(
 	error_stack[0] = err;
 }
 
-void ClearError()
+static void ClearError()
 {
 	error_info_t &err = error_stack[0];
 	int32_t i = 0;
@@ -61,7 +83,7 @@ void ClearError()
 	}
 }
 
-uint32_t GetNumErrors()
+static uint32_t GetNumErrors()
 {
 	int32_t i = 0;
 	while (error_stack[i].valid) ++i;
